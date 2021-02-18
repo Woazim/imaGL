83ca337887223bab9ab3f4f6459fe8d9f4a02af3
Benjamin ALBOUY-KISSI <b.albouy.kissi@gmail.com>
2021-02-18 01:41:04 +0100

    First attempt for jpeg decompression. Not tested yet.

---
+++ imaGL/loader_jpeg.cpp
@@ -0,0 +1,80 @@
+#include "loader_jpeg.h"
+#ifdef _HAS_JPEG
+
+#include "privateimagldata.h"
+#include <cstring>
+#include <algorithm>
+#include <bit>
+#include <vector>
+
+namespace imaGL {
+
+  SPrivateImaGLData imaGL::CLoaderJPEG::load(std::istream& is)
+  {
+    SPrivateImaGLData ret;
+
+    //Create jpeg decompressor and error handler
+    jpeg_decompress_struct cinfo;
+    my_error_mgr jerr;
+    cinfo.err = jpeg_std_error(&jerr.pub);
+    jerr.pub.error_exit = my_error_exit;
+    if (setjmp(jerr.setjmp_buffer)) {
+      /* If we get here, the JPEG code has signaled an error.
+       * We need to clean up the JPEG object, and throw an exception.
+       */
+      jpeg_destroy_decompress(&cinfo);
+      throw bad_format("libJPEG could not open your jpeg data!");
+    }
+
+    jpeg_create_decompress(&cinfo);
+
+    //Load jpeg data in memory
+    is.seekg(0, std::ios_base::end);
+    size_t bufLength = is.tellg();
+    is.seekg(0, std::ios_base::beg);
+    std::vector<char> jpegBuffer(bufLength);
+    is.read(jpegBuffer.data(), bufLength);
+
+    //Attach this memory buffer to jpeg decompressor
+    jpeg_mem_src(&cinfo, reinterpret_cast<unsigned char*>(jpegBuffer.data()), bufLength);
+
+    //Read jpeg header
+    jpeg_read_header(&cinfo, TRUE);
+
+    //Set decompression options
+    cinfo.dct_method = JDCT_ISLOW;
+
+    //Start jpeg decompression
+    jpeg_start_decompress(&cinfo);
+
+    //Initialize SPrivateImaGLData
+    ret.m_nHeight = cinfo.output_height;
+    ret.m_nWidth = cinfo.output_width;
+    if (cinfo.output_components == 1)
+      ret.m_PixelFormat = CImaGL::EPixelFormat::R;
+    else
+      ret.m_PixelFormat = CImaGL::EPixelFormat::RGB;
+    ret.m_PixelType = CImaGL::EPixelType::UByte;
+    ret.m_nPixelSize = computePixelSize(ret.m_PixelFormat, ret.m_PixelType);
+    ret.m_vRawData.resize(ret.m_nHeight * ret.m_nWidth * ret.m_nPixelSize);
+
+    std::vector<std::byte*> row_pointers;
+    row_pointers.resize(ret.m_nHeight);
+    //Flip image since OpenGL expects texture to be read from bottom to top
+    for (size_t i = 0; i < ret.m_nHeight; ++i)
+      row_pointers[ret.m_nHeight - i - 1] = ret.m_vRawData.data() + i * ret.m_nWidth * ret.m_nPixelSize;
+
+    //Read jpeg scanlines
+    while (cinfo.output_scanline < cinfo.output_height)
+      jpeg_read_scanlines(&cinfo, reinterpret_cast<JSAMPARRAY>(row_pointers.data() + cinfo.output_scanline), ret.m_nHeight);
+    
+    //Be polite and free memory
+    jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
+
+    return ret;
+  }
+
+}
+
+#endif
---
+++ imaGL/loader_jpeg.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "loader.h"
+#include "public/imaGL/imaGL.h"
+
+#ifdef _HAS_JPEG
+#include <jpeglib.h>
+#include <csetjmp>
+
+namespace imaGL {
+
+  class CLoaderJPEG :
+    public ILoader
+  {
+    struct my_error_mgr {
+      struct jpeg_error_mgr pub;	/* "public" fields */
+
+      jmp_buf setjmp_buffer;	/* for return to caller */
+    };
+    static void my_error_exit(j_common_ptr cinfo)
+    {
+      /* cinfo->err really points to a my_error_mgr struct, so coerce pointer */
+      my_error_mgr* myerr = (my_error_mgr*)cinfo->err;
+
+      /* Always display the message. */
+      /* We could postpone this until after returning, if we chose. */
+      (*cinfo->err->output_message) (cinfo);
+
+      /* Return control to the setjmp point */
+      longjmp(myerr->setjmp_buffer, 1);
+    }
+
+  public:
+    SPrivateImaGLData load(std::istream& is) override;
+  };
+
+}
+
+#endif
